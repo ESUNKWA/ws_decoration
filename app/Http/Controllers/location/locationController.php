22cf3577bedf93bc7cd7e022bb9edf5ca7aecd53
ESUNKWA <kouadiodeki@gmail.com>
2022-05-04 22:20:48 +0000

    update location

--- app/Http/Controllers/location/locationController.php
+++ app/Http/Controllers/location/locationController.php
@@ -43,7 +43,8 @@ class locationController extends Controller
                 ->join('t_locations', 't_clients.r_i', '=', 't_locations.r_client')
                 ->join('t_communes', 't_communes.r_i', '=', 't_locations.r_destination')
                 ->leftJoin('t_logistiques', 't_logistiques.r_i', '=', 't_locations.r_logistik')
-                ->select('t_clients.*','t_locations.*','t_communes.r_libelle as destination','t_logistiques.r_vehicule','t_logistiques.r_matricule')
+                ->select('t_clients.r_i as idclient','t_clients.r_nom','t_clients.r_prenoms','t_clients.r_telephone','t_clients.r_email','t_clients.r_description','t_clients.r_utilisateur',
+                't_locations.*','t_communes.r_libelle as destination','t_logistiques.r_vehicule','t_logistiques.r_matricule')
                 ->where('t_locations.r_status', DB::raw('COALESCE('.$inputs['p_status'].',t_locations.r_status)'))
                 ->whereDate('t_locations.r_date_envoie', '=', $inputs['p_date'])
                 ->get();
@@ -200,6 +201,97 @@ class locationController extends Controller
                             $response = [
                                 '_status' => 1,
                                 '_result' => 'Enregistrement effectuée avec succès'
+                            ];
+                            return response()->json($response, 200);
+
+                } catch (\Throwable $e) {
+                    DB::rollBack(); //Annulation de la transaction
+                    return $e->getMessage();
+                }
+        }
+    }
+
+    public function modif_location(Request $request)
+    {
+
+        $inputs = $request->all();
+
+        $errors = [
+            'p_details'  => 'required',
+            'p_date_envoie'  => 'required',
+            'p_date_retour'  => 'required',
+            'p_commune_depart'  => 'required',
+            'p_commune_arrive'  => 'required',
+
+            'p_mnt_total'  => 'required',
+            //'p_utilisateur'  => 'required',
+        ];
+
+        $erreurs = [
+            'p_details.required' => 'Veuillez saisir les détails de la location',
+            
+            'p_date_envoie.required' => 'Veuillez saisir la date d\'envoie',
+            'p_date_retour.required' => 'Veuillez saisir la date de retour',
+            'p_commune_depart.required' => 'La commune de départ est inconnue',
+            'p_commune_arrive.required' => 'La destination est inconnue',
+            'p_mnt_total.required' => 'Le montant du total de la location est inconnu',
+            //'p_utilisateur.required' => 'L\'utilisateur est inconnue'
+        ];
+
+        $validator = Validator::make($inputs, $errors, $erreurs);
+
+        if( $validator->fails()){
+            return $validator->errors();
+        }else{
+            $date = date('ym');
+            
+                //Insertion des données du client
+                try {
+                    DB::beginTransaction();
+                    $location = Location::find($inputs['p_idlocation']);
+               
+                            //Insertion des données sur la location
+                            $location->update([
+                                //'r_client' => $insertion->r_i,
+                                //'r_num' => ($insertion->r_i < 9 )? $date.'0'.$insertion->r_i : $date.$insertion->r_i ,
+                                'r_mnt_total' => $request->p_mnt_total,
+                                
+                                'r_frais_transport' => $request->p_frais,
+                                'r_destination' => $request->p_commune_arrive,
+                                'r_remise' => $request->p_remise,
+                                'r_mnt_total_remise' =>  $request->p_mnt_total,
+                                'r_logistik' => $request->p_vehicule,
+                                'r_date_envoie' => $request->p_date_envoie,
+                                'r_date_retour' => $request->p_date_retour,
+                                'r_duree' => $request->p_duree,
+                            ]);
+                          
+                           $detail_location = Detailslocacation::where('r_location',$inputs['p_idlocation'])->delete();
+                           
+                           if ( $detail_location !== 0) {
+                               //Insertion des détails de la la location
+                                for ($i=0; $i < count($request->p_details); $i++) {
+
+                                    $insertion_details = Detailslocacation::create([
+                                        'r_quantite' => $request->p_details[$i]["qte"],
+                                        'r_produit' => $request->p_details[$i]["idproduit"],
+                                        'r_location' => $location->r_i,
+                                        'r_sous_total' => $request->p_details[$i]["total"],
+                                        'r_utilisateur' => $request->p_utilisateur
+                                        //'r_prix_unitaire' => 1000
+                                    ]);
+                                }
+                           }
+
+                            
+
+                            $request['p_location'] = $location->r_i;
+                            //$res = $this->majstockProduit($request);
+
+                            DB::commit();// Commit
+                            $response = [
+                                '_status' => 1,
+                                '_result' => 'Modification effectuée avec succès'
                             ];
                             return response()->json($response, 200);
 
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\authController;
 use App\Http\Controllers\dashController;
 use App\Http\Controllers\categorieController;
 use App\Http\Controllers\utilisateursController;
+use App\Http\Controllers\metier\clientController;
 use App\Http\Controllers\metier\communesController;
 use App\Http\Controllers\metier\logistikController;
 use App\Http\Controllers\metier\produitsController;
@@ -13,8 +14,8 @@ use App\Http\Controllers\ProfilUtilisatersController;
 use App\Http\Controllers\personnel\fonctionController;
 use App\Http\Controllers\metier\achatproduitController;
 use App\Http\Controllers\metier\tarificationController;
-use App\Http\Controllers\location\detailsLocationController;
 use App\Http\Controllers\personnel\PersonnelController;
+use App\Http\Controllers\location\detailsLocationController;
 
 Route::put('profil/edit/{idprofil}', [ProfilUtilisatersController::class, 'modif']);
 Route::post('location/{proforma}', [locationController::class, 'store']);
@@ -28,6 +29,8 @@ Route::post('tarifapply', [tarificationController::class, 'tarifapply']);
 Route::post('paymentpartiel', [locationController::class, 'add_payment']);
 Route::post('tarification_cibles', [tarificationController::class, 'tarification_cibles']);
 Route::post('deconnect', [authController::class, 'deconnect']);
+Route::post('updatelocation', [locationController::class, 'modif_location']);
+
 Route::resources([
     'profils'    => ProfilUtilisatersController::class,
     'auth'    => authController::class,
@@ -44,4 +47,6 @@ Route::resources([
     //Personnel
     'fonction' => fonctionController::class,
     'personnel' => PersonnelController::class,
+    //Client
+    'clients' => clientController::class,
 ]);
