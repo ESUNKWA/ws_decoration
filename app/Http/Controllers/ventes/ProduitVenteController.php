36a05489bc5d483aa207015dc26b76508ce166ef
ESUNKWA <kouadiodeki@gmail.com>
2022-08-21 13:54:34 +0000

    Route pour la saisie et la modification des produits à vendre

--- app/Http/Controllers/ventes/ProduitVenteController.php
+++ app/Http/Controllers/ventes/ProduitVenteController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Traits\cryptTrait;
 use App\Http\Traits\ResponseTrait;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
 use App\Models\VenteProduits\ProduitsVente as Produits;
 
 class ProduitVenteController extends Controller
@@ -47,7 +48,42 @@ class ProduitVenteController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        //Décryptage des données récues
+       //$inputs = $this->decryptData($request->p_data);
+       $inputs = $request->p_data;
+
+       // Validation des champs
+       $errors = [
+           'r_nom_produit'  => 'required|unique:t_produitventes',
+           'r_creer_par' => 'required',
+           'r_modifier_par' => 'required',
+       ];
+       $erreurs = [
+           'r_nom_produit.required' =>'Le nom du produit est obligatoire',
+           'r_nom_produit.unique' =>'Le nom du produit existe dejà',
+           'r_creer_par.required'  => 'Utilisateur obligatoire',
+           'r_modifier_par.required'  => 'Utilisateur obligatoire'
+       ];
+
+       $validate = Validator::make($inputs, $errors, $erreurs);
+
+       if( $validate->fails()){
+           return $this->responseCatchError($validate->errors());
+       }else{
+
+           try {
+
+               $insertion = Produits::create($inputs);
+
+               $response = $this->crypt('Le produit [ '.$insertion->r_nom_produit.' ] à bien été enregistrée');
+
+               return $this->responseSuccess($response);
+
+           } catch (\Throwable $e) {
+               return $this->responseCatchError($e->getMessage());
+           }
+
+       }
     }
 
     /**
@@ -79,9 +115,43 @@ class ProduitVenteController extends Controller
      * @param  \App\Models\c  $c
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, c $c)
+    public function update(Request $request, int $id)
     {
-        //
+        //$inputs = $this->decryptData($request->p_data);
+       $inputs = $request->p_data;
+
+       // Validation des champs
+       $errors = [
+        'r_nom_produit'  => 'required',
+        'r_creer_par' => 'required',
+        'r_modifier_par' => 'required',
+        ];
+        $erreurs = [
+            'r_nom_produit.required' =>'Le nom du produit est obligatoire',
+            'r_creer_par.required'  => 'Utilisateur obligatoire',
+            'r_modifier_par.required'  => 'Utilisateur obligatoire'
+        ];
+
+       $validate = Validator::make($inputs, $errors, $erreurs);
+
+        if( $validate->fails()){
+            $response = [
+                '_status' =>0,
+                '_result' => $validate->errors()
+            ];
+            return $response;
+        }else{
+
+            $update = Produits::find($id);
+
+            $update->update($inputs);
+
+            $response = [
+                '_status' => 1,
+                '_result' => 'Le produit [ '.$update->r_nom_produit.' ] à bien été modifiée'
+            ];
+            return response()->json($response, 200);
+        }
     }
 
     /**
